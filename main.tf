01d17272c5635cd0248fc7d2fb9e07a4c20279de
SUNANDA SRI KARUMURI <Karumuri2003@gmail.com>
2026-07-29 13:57:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "ami-02b64aa047cb5edf5"
+    instance_type = "t3.micro"
     tags = {
-      Name = "nitnit"
+      Name = "sri-ec2"
     }
 }
